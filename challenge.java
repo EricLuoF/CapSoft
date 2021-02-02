f60afc6a5fdfbd60d70e158e856c29af88abb88e
EricLuoF <eric.luo@utexas.edu>
2021-02-01 22:19:27 -0500

    Config 1.1

--- challenge.java
+++ challenge.java
@@ -11,40 +11,43 @@ import java.util.Queue;
 public class challenge {
 
     public static void main(String[] args)throws FileNotFoundException{
-        boolean debug = true;
+        boolean debug = false;
 
+        //User testing from file
         //Reads in from file for testing
-        Scanner scann = new Scanner(System.in);
-        System.out.println("Enter Input File Location:");
-        String filename = scann.nextLine();
-        scann.close();
+        if(debug){
+            Scanner scanfile = new Scanner(System.in);
+            System.out.println("Enter Input File Location:");
+            String filename = scanfile.nextLine();
+            scanfile.close();
+            //Clears the terminal screen
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
         File in = new File(filename);
-        //File in = new File("C:\\Users\\Eric\\Desktop\\Code\\captivationchallengejava\\input.txt");
         Scanner scan = new Scanner(in);
         scan.useDelimiter("");
 
-        //Actual scanner, should both work
-        //Scanner scan = new Scanner(System.in);
-        //scan.useDelimiter("");
-
         challenge test = new challenge();
-        char check;
 
         //C = 01000011
         @SuppressWarnings("unchecked")
         Queue<Character> queue = test.setup(scan);
 
-        //since in theory this is a never ending stream
+        //Runs as long as the file has more data within it
         while(scan.hasNext()){
-            //if these 8 bits are = C
+            
+            //Queue of 8 bits that is constantly checking to see if its 'C', else it bit shifts to the right by 1.
             if(test.bitCheck(queue)){
+                //checkFlag is called after a 'C' is detected, and the following letters are checked to see if CAPTIVATION is written
                 if (test.checkFlag(scan)){
+                    //If there is indeed the CAPTIVATION flag, then print the next 100 characters, then go back to polling for 'C'
                     test.printHundred(scan);
                 }
             }
-            //Move forward by 1 bit
+            //Shifts the queue to the right by 1 bit
             if(scan.hasNext()) {
-                queue.poll();
+                queue.remove();
                 queue.add(scan.next().charAt(0));
             }
 
